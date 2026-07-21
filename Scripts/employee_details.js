93476da3c33221ac4dc452acea9910336634a862
Benjamin Asjali <beasjali@addu.edu.ph>
2026-07-21 13:52:12 +0800

    feat: added new function for employees

--- Scripts/employee_details.js
+++ Scripts/employee_details.js
@@ -1,10 +1,10 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-      { id: 4, name: 'Benjamin Asjali', age: 23, department: 'IT', salary: 80000 },
-      { id: 5, name: 'Brandon Santos', age: 28, department: 'Finance', salary: 70000 },
-      { id: 6, name: 'Benz Pilapil', age: 22, department: 'HR', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: "Javascript" },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: "HTML" },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: "HTML" },
+      { id: 4, name: 'Benjamin Asjali', age: 23, department: 'IT', salary: 80000, specialization: "CSS" },
+      { id: 5, name: 'Brandon Santos', age: 28, department: 'Finance', salary: 70000, specialization:"Python" },
+      { id: 6, name: 'Benz Pilapil', age: 22, department: 'HR', salary: 60000, specialization: "Javascript"},
     ];
 
 function displayEmployees() {
@@ -34,4 +34,10 @@ function findEmployeeById(employeeId) {
   } else {
     document.getElementById('employeesDetails').innerHTML = 'There are no employees with the number you placed';
   }
+}
+
+function specializationEmployees(employeeSpecial) {
+  const findSpecial = employees.filter(employee => employee.specialization === "Javascript");
+  const foundSpecial = findSpecial.map(employee => `<p>${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - ${employee.salary} - ${employee.specialization} </p>`).join('')
+  document.getElementById('employeesDetails').innerHTML = foundSpecial;
 }
